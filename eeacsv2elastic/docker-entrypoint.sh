08e8c0db919b495df8195450c8f8bf8e76f953ac
Mauro Michielon <mauro.michielon@eea.europa.eu>
2017-02-23 11:20:51 +0100

    Update docker-entrypoint.sh

--- eeacsv2elastic/docker-entrypoint.sh
+++ eeacsv2elastic/docker-entrypoint.sh
@@ -3,8 +3,8 @@
 set -e
 
 if [ "$LOGSTASH_RW_USERNAME" ]; then
-    sed "s#LOGSTASH_RW_USERNAME#$LOGSTASH_RW_USERNAME#g" -i /root/ingest.js
-    sed "s#LOGSTASH_RW_PASSWORD#$LOGSTASH_RW_PASSWORD#g" -i /root/ingest.js
+    sed "s#RW_USERNAME#$RW_USERNAME#g" -i /root/ingest.js
+    sed "s#RW_PASSWORD#$RW_PASSWORD#g" -i /root/ingest.js
 fi
 
 sed "s#INDEXNAME#$INDEXNAME#g" -i /root/ingest.js
