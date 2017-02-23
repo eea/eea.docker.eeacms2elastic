7535628a1534efe59e036fa582821bc3864e67eb
michimau <mauro.michielon@eea.europa.eu>
2017-02-23 13:20:34 +0100

    parameters substitution

--- eeacsv2elastic/docker-entrypoint.sh
+++ eeacsv2elastic/docker-entrypoint.sh
@@ -5,11 +5,17 @@ set -e
 if [ "$LOGSTASH_RW_USERNAME" ]; then
     sed "s#RW_USERNAME#$RW_USERNAME#g" -i /root/ingest.js
     sed "s#RW_PASSWORD#$RW_PASSWORD#g" -i /root/ingest.js
+    sed "s#RW_USERNAME#$RW_USERNAME#g" -i /opt/script.sh
+    sed "s#RW_PASSWORD#$RW_PASSWORD#g" -i /opt/script.sh
+
 fi
 
 sed "s#INDEXNAME#$INDEXNAME#g" -i /root/ingest.js
-sed "s#INDEXNAME#$INDEXNAME#g" -i /root/ingest.js
 sed "s#OUTPUTDIR#$OUTPUTDIR#g" -i /root/ingest.js
+
+sed "s#INDEXNAME#$INDEXNAME#g" -i /opt/script.sh
+sed "s#OUTPUTDIR#$OUTPUTDIR#g" -i /opt/script.sh
+sed "s#DOWNLOADURL#$DOWNLOADURL#g" -i /opt/script.sh
 
 exec "$@" 
 
--- eeacsv2elastic/script.sh
+++ eeacsv2elastic/script.sh
@@ -2,16 +2,16 @@
 
 set -e
 
-wget $DOWNLOADURL -O /$OUTPUTDIR/$INDEXNAME.csv 
+wget DOWNLOADURL -O /OUTPUTDIR/INDEXNAME.csv 
 
-sed "s#:text##g" -i /$OUTPUTDIR/$INDEXNAME.csv
-sed "s#:date##g" -i /$OUTPUTDIR/$INDEXNAME.csv
-sed "s#:url##g" -i /$OUTPUTDIR/$INDEXNAME.csv
-sed "s#:number##g" -i /$OUTPUTDIR/$INDEXNAME.csv
+sed "s#:text##g" -i /OUTPUTDIR/INDEXNAME.csv
+sed "s#:date##g" -i /OUTPUTDIR/INDEXNAME.csv
+sed "s#:url##g" -i /OUTPUTDIR/INDEXNAME.csv
+sed "s#:number##g" -i /OUTPUTDIR/INDEXNAME.csv
 
 if [ -s /$OUTPUTDIR/$INDEXNAME.csv ]
 then
-	curl -XPOST 'http://$RW_USERNAME:$RW_PASSWORD@elasticsearch:9200/$INDEXNAME/logs/_delete_by_query?conflicts=proceed&pretty' -d'{ "query": { "match_all": {} } }'
+	curl -XPOST 'http://RW_USERNAME:RW_PASSWORD@elasticsearch:9200/INDEXNAME/logs/_delete_by_query?conflicts=proceed&pretty' -d'{ "query": { "match_all": {} } }'
 	node /root/ingest.js
 else
     echo "File empty"
