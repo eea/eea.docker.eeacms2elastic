f28fc0fef2d9215b1c23c11a15071d461e7f955c
michimau <mauro.michielon@eea.europa.eu>
2017-02-23 13:37:13 +0100

    embarassing error

--- eeacsv2elastic/script.sh
+++ eeacsv2elastic/script.sh
@@ -11,8 +11,8 @@ sed "s#:number##g" -i /OUTPUTDIR/INDEXNAME.csv
 
 if [ -s /$OUTPUTDIR/$INDEXNAME.csv ]
 then
-	curl -XPOST 'http://RW_USERNAME:RW_PASSWORD@elasticsearch:9200/INDEXNAME/logs/_delete_by_query?conflicts=proceed&pretty' -d'{ "query": { "match_all": {} } }'
-	node /root/ingest.js
+	echo "File empty"
 else
-    echo "File empty"
+    curl -XPOST 'http://RW_USERNAME:RW_PASSWORD@elasticsearch:9200/INDEXNAME/logs/_delete_by_query?conflicts=proceed&pretty' -d'{ "query": { "match_all": {} } }'
+	node /root/ingest.js
 fi
