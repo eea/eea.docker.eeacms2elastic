7809511fd16b862f727758e6d3f1093b2ba2d30c
Mauro Michielon - EEA 06Oct2015 <michimau@dev-mil-23.pecs.eea>
2017-05-04 12:03:16 +0200

    wip

--- eeacsv2elastic/docker-entrypoint.sh
+++ eeacsv2elastic/docker-entrypoint.sh
@@ -7,14 +7,32 @@ if [ "$RW_USERNAME" ]; then
     sed "s#RW_PASSWORD#$RW_PASSWORD#g" -i /root/ingest.js
     sed "s#RW_USERNAME#$RW_USERNAME#g" -i /opt/script.sh
     sed "s#RW_PASSWORD#$RW_PASSWORD#g" -i /opt/script.sh
+    sed "s#RW_USERNAME#$RW_USERNAME#g" -i /opt/ingestKibana.sh
+    sed "s#RW_PASSWORD#$RW_PASSWORD#g" -i /opt/ingestKibana.sh
 fi
 
 sed "s#INDEXNAME#$INDEXNAME#g" -i /root/ingest.js
 sed "s#OUTPUTDIR#$OUTPUTDIR#g" -i /root/ingest.js
-
 sed "s#INDEXNAME#$INDEXNAME#g" -i /opt/script.sh
 sed "s#OUTPUTDIR#$OUTPUTDIR#g" -i /opt/script.sh
+
 sed "s#DOWNLOADURL#$DOWNLOADURL#g" -i /opt/script.sh
+
+sed "s#KIBANACONFIGURATIONDIR#$KIBANACONFIGURATIONDIR#g" -i /opt/script.sh
+sed "s#KIBANACONFIGURATIONDIR#$KIBANACONFIGURATIONDIR#g" -i /opt/ingestKibana.sh
+sed "s#INDEXNAME#$INDEXNAME#g" -i /root/ingestKibana.sh
+
+if [ $RESETATSTARTUP = "YES" ]
+then
+    rm -rf $KIBANACONFIGURATIONDIR/$INDEXNAME
+fi
+
+#next 3 lines will fail if the directory is already there
+git clone https://github.com/eea/eea.kibana.configs.git $KIBANACONFIGURATIONDIR/$INDEXNAME
+cd $KIBANACONFIGURATIONDIR/$INDEXNAME
+git checkout $INDEXNAME
+
+sh /opt/ingestKibana.sh
 
 exec "$@" 
 
--- eeacsv2elastic/script.sh
+++ eeacsv2elastic/script.sh
@@ -9,10 +9,19 @@ sed "s#:date##g" -i /OUTPUTDIR/INDEXNAME.csv
 sed "s#:url##g" -i /OUTPUTDIR/INDEXNAME.csv
 sed "s#:number##g" -i /OUTPUTDIR/INDEXNAME.csv
 
+elasticdump --input=http://RW_USERNAME:RW_PASSWORD@elasticsearch:9200/INDEXNAME --output=$ --type=mapping | grep INDEXNAME > KIBANACONFIGURATIONDIR/INDEXNAME/INDEXNAME_mapping.json
+elasticdump --input=http://RW_USERNAME:RW_PASSWORD@elasticsearch:9200/INDEXNAME --output=$ --type=analyzer | grep INDEXNAME > KIBANACONFIGURATIONDIR/INDEXNAME/INDEXNAME_analyzer.json
+#elasticdump --input=http://RW_USERNAME:RW_PASSWORD@elasticsearch:9200/INDEXNAME --output=$ --type=data | grep INDEXNAME > KIBANACONFIGURATIONDIR/INDEXNAME/INDEXNAME_data.json
+
+elasticdump --input=http://RW_USERNAME:RW_PASSWORD@elasticsearch:9200/.kibana --output=$ --type=mapping  | grep INDEXNAME > KIBANACONFIGURATIONDIR/INDEXNAME/INDEXNAME_kibana_mapping.json 
+elasticdump --input=http://RW_USERNAME:RW_PASSWORD@elasticsearch:9200/kibana --output=$ --type=analyzer  | grep INDEXNAME > KIBANACONFIGURATIONDIR/INDEXNAME/INDEXNAME_kibana_analyzer.json
+elasticdump --input=http://RW_USERNAME:RW_PASSWORD@elasticsearch:9200/.kibana --output=$ --type=data  | grep INDEXNAME > KIBANACONFIGURATIONDIR/INDEXNAME/INDEXNAME_kibana_data.json
+
 if [ -s /$OUTPUTDIR/$INDEXNAME.csv ]
 then
-	echo "File empty"
+    echo "File empty"
 else
     curl -XPOST 'http://RW_USERNAME:RW_PASSWORD@elasticsearch:9200/INDEXNAME/logs/_delete_by_query?conflicts=proceed&pretty' -d'{ "query": { "match_all": {} } }'
-	node /root/ingest.js
+    node /root/ingest.js
 fi
+
