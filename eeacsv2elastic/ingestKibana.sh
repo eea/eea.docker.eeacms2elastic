f9fc3994dd63a1ace56d39c11c573323d943646c
Mauro Michielon - EEA 06Oct2015 <michimau@dev-mil-26.pecs.eea>
2017-05-05 10:37:47 +0200

    configurations from https://github.com/eea/eea.kibana.configs

--- eeacsv2elastic/ingestKibana.sh
+++ eeacsv2elastic/ingestKibana.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-elasticdump --input=KIBANACONFIGURATIONDIR/INDEXNAME/INDEXNAME_kibana_mapping.json --output=http://RW_USERNAME:RW_PASSWORD@elasticsearch:9200/.kibana
-elasticdump --input=KIBANACONFIGURATIONDIR/INDEXNAME/INDEXNAME_kibana_analyzer.json --output=http://RW_USERNAME:RW_PASSWORD@elasticsearch:9200/kibana
-elasticdump --input=KIBANACONFIGURATIONDIR/INDEXNAME/INDEXNAME_kibana_data.json --output=http://RW_USERNAME:RW_PASSWORD@elasticsearch:9200/.kibana
+elasticdump --input=KIBANACONFIGURATIONDIR/INDEXNAME/kibana_mapping.json --output=http://RW_USERNAME:RW_PASSWORD@elasticsearch:9200/.kibana
+elasticdump --input=KIBANACONFIGURATIONDIR/INDEXNAME/kibana_analyzer.json --output=http://RW_USERNAME:RW_PASSWORD@elasticsearch:9200/kibana
+elasticdump --input=KIBANACONFIGURATIONDIR/INDEXNAME/kibana_data.json --output=http://RW_USERNAME:RW_PASSWORD@elasticsearch:9200/.kibana
 
-elasticdump --input=KIBANACONFIGURATIONDIR/INDEXNAME/INDEXNAME_mapping.json --output=http://RW_USERNAME:RW_PASSWORD@elasticsearch:9200/INDEXNAME
-elasticdump --input=KIBANACONFIGURATIONDIR/INDEXNAME/INDEXNAME_analyzer.json --output=http://RW_USERNAME:RW_PASSWORD@elasticsearch:9200/INDEXNAME
+#elasticdump --input=KIBANACONFIGURATIONDIR/INDEXNAME/INDEXNAME_mapping.json --output=http://RW_USERNAME:RW_PASSWORD@elasticsearch:9200/INDEXNAME
+#elasticdump --input=KIBANACONFIGURATIONDIR/INDEXNAME/INDEXNAME_analyzer.json --output=http://RW_USERNAME:RW_PASSWORD@elasticsearch:9200/INDEXNAME
 #elasticdump --input=KIBANACONFIGURATIONDIR/INDEXNAME_data.json --output=http://RW_USERNAME:RW_PASSWORD@elasticsearch:9200/INDEXNAME
--- eeacsv2elastic/script.sh
+++ eeacsv2elastic/script.sh
@@ -9,19 +9,19 @@ sed "s#:date##g" -i /OUTPUTDIR/INDEXNAME.csv
 sed "s#:url##g" -i /OUTPUTDIR/INDEXNAME.csv
 sed "s#:number##g" -i /OUTPUTDIR/INDEXNAME.csv
 
-elasticdump --input=http://RW_USERNAME:RW_PASSWORD@elasticsearch:9200/INDEXNAME --output=$ --type=mapping | grep INDEXNAME > KIBANACONFIGURATIONDIR/INDEXNAME/INDEXNAME_mapping.json
-elasticdump --input=http://RW_USERNAME:RW_PASSWORD@elasticsearch:9200/INDEXNAME --output=$ --type=analyzer | grep INDEXNAME > KIBANACONFIGURATIONDIR/INDEXNAME/INDEXNAME_analyzer.json
-#elasticdump --input=http://RW_USERNAME:RW_PASSWORD@elasticsearch:9200/INDEXNAME --output=$ --type=data | grep INDEXNAME > KIBANACONFIGURATIONDIR/INDEXNAME/INDEXNAME_data.json
+#elasticdump --input=http://RW_USERNAME:RW_PASSWORD@elasticsearch:9200/INDEXNAME --output=$ --type=mapping | grep INDEXNAME > KIBANACONFIGURATIONDIR/INDEXNAME/mapping.json
+#elasticdump --input=http://RW_USERNAME:RW_PASSWORD@elasticsearch:9200/INDEXNAME --output=$ --type=analyzer | grep INDEXNAME > KIBANACONFIGURATIONDIR/INDEXNAME/analyzer.json
+#elasticdump --input=http://RW_USERNAME:RW_PASSWORD@elasticsearch:9200/INDEXNAME --output=$ --type=data | grep INDEXNAME > KIBANACONFIGURATIONDIR/INDEXNAME/data.json
 
-elasticdump --input=http://RW_USERNAME:RW_PASSWORD@elasticsearch:9200/.kibana --output=$ --type=mapping  | grep INDEXNAME > KIBANACONFIGURATIONDIR/INDEXNAME/INDEXNAME_kibana_mapping.json 
-elasticdump --input=http://RW_USERNAME:RW_PASSWORD@elasticsearch:9200/kibana --output=$ --type=analyzer  | grep INDEXNAME > KIBANACONFIGURATIONDIR/INDEXNAME/INDEXNAME_kibana_analyzer.json
-elasticdump --input=http://RW_USERNAME:RW_PASSWORD@elasticsearch:9200/.kibana --output=$ --type=data  | grep INDEXNAME > KIBANACONFIGURATIONDIR/INDEXNAME/INDEXNAME_kibana_data.json
+elasticdump --input=http://RW_USERNAME:RW_PASSWORD@elasticsearch:9200/.kibana --output=$ --type=mapping  > KIBANACONFIGURATIONDIR/INDEXNAME/kibana_mapping.json 
+elasticdump --input=http://RW_USERNAME:RW_PASSWORD@elasticsearch:9200/kibana --output=$ --type=analyzer  > KIBANACONFIGURATIONDIR/INDEXNAME/kibana_analyzer.json
+elasticdump --input=http://RW_USERNAME:RW_PASSWORD@elasticsearch:9200/.kibana --output=$ --type=data  > KIBANACONFIGURATIONDIR/INDEXNAME/kibana_data.json
 
 if [ -s /$OUTPUTDIR/$INDEXNAME.csv ]
 then
     echo "File empty"
 else
     curl -XPOST 'http://RW_USERNAME:RW_PASSWORD@elasticsearch:9200/INDEXNAME/logs/_delete_by_query?conflicts=proceed&pretty' -d'{ "query": { "match_all": {} } }'
-    node /root/ingest.js
+    node /opt/ingest.js
 fi
 
