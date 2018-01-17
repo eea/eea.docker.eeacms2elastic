9ccb4a22c7c50a1c28f91a846f68a72fa844ad6d
Mauro Michielon <mauro.michielon@eea.europa.eu>
2018-01-17 19:19:24 +0100

    proper header set for elasticdump

--- eeacsv2elastic/docker-entrypoint.sh
+++ eeacsv2elastic/docker-entrypoint.sh
@@ -13,7 +13,7 @@ sed "s#INDEXNAME#$INDEXNAME#g" -i /opt/script.sh
 
 counter=0
 #while [ ! "$(curl -k http://$RW_USERNAME:$RW_PASSWORD@localhost:9200 2> /dev/null)" -a $counter -lt 100 ]; do
-while [ ! "$(curl -k https://$RW_USERNAME:$RW_PASSWORD@elasticsearch:9200 2> /dev/null)" -a $counter -lt 100  ]; do
+while [ ! "$(curl -k --headers='{"Content-Type": "application/json"}' https://$RW_USERNAME:$RW_PASSWORD@elasticsearch:9200 2> /dev/null)" -a $counter -lt 100  ]; do
   sleep 5
   let counter=counter+1
   echo "waiting for Elasticsearch to be up ($counter/100)"
@@ -25,9 +25,9 @@ then
   git clone https://github.com/eea/eea.kibana.configs.git /eea.kibana.configs
   if [[ -d "/eea.kibana.configs/$INDEXNAME" ]];
     then
-      NODE_TLS_REJECT_UNAUTHORIZED=0 elasticdump --input=/eea.kibana.configs/$INDEXNAME/kibana_mapping.json --output=https://$LOGSTASH_RW_USERNAME:$LOGSTASH_RW_PASSWORD@elasticsearch:9200/.kibana
-      NODE_TLS_REJECT_UNAUTHORIZED=0 elasticdump --input=/eea.kibana.configs/$INDEXNAME/kibana_analyzer.json --output=https://$LOGSTASH_RW_USERNAME:$LOGSTASH_RW_PASSWORD@elasticsearch:9200/.kibana
-      NODE_TLS_REJECT_UNAUTHORIZED=0 elasticdump --input=/eea.kibana.configs/$INDEXNAME/kibana_data.json --output=https://$LOGSTASH_RW_USERNAME:$LOGSTASH_RW_PASSWORD@elasticsearch:9200/.kibana
+      NODE_TLS_REJECT_UNAUTHORIZED=0 elasticdump --headers='{"Content-Type": "application/json"}' --input=/eea.kibana.configs/$INDEXNAME/kibana_mapping.json --output=https://$LOGSTASH_RW_USERNAME:$LOGSTASH_RW_PASSWORD@elasticsearch:9200/.kibana
+      NODE_TLS_REJECT_UNAUTHORIZED=0 elasticdump --headers='{"Content-Type": "application/json"}' --input=/eea.kibana.configs/$INDEXNAME/kibana_analyzer.json --output=https://$LOGSTASH_RW_USERNAME:$LOGSTASH_RW_PASSWORD@elasticsearch:9200/.kibana
+      NODE_TLS_REJECT_UNAUTHORIZED=0 elasticdump --headers='{"Content-Type": "application/json"}' --input=/eea.kibana.configs/$INDEXNAME/kibana_data.json --output=https://$LOGSTASH_RW_USERNAME:$LOGSTASH_RW_PASSWORD@elasticsearch:9200/.kibana
   fi
 else
   cd /eea.kibana.configs
--- eeacsv2elastic/script.sh
+++ eeacsv2elastic/script.sh
@@ -35,8 +35,9 @@ export TESTKIBANAINDEX=$(curl -k --user $LOGSTASH_RW_USERNAME:$LOGSTASH_RW_PASSW
 if [[ ! -z "$TESTKIBANAINDEX" ]]
 then
   mkdir -p /$KIBANACONFIGURATIONDIR/INDEXNAME
-  NODE_TLS_REJECT_UNAUTHORIZED=0 elasticdump -XPOST -H'Content-Type: application/json' --input="https://$LOGSTASH_RW_USERNAME:$LOGSTASH_RW_PASSWORD@elasticsearch:9200/.kibana" --output=$ --type=mapping   > /$KIBANACONFIGURATIONDIR/INDEXNAME/kibana_mapping.json
-  NODE_TLS_REJECT_UNAUTHORIZED=0 elasticdump -XPOST -H'Content-Type: application/json' --input="https://$LOGSTASH_RW_USERNAME:$LOGSTASH_RW_PASSWORD@elasticsearch:9200/.kibana" --output=$ --type=analyzer  > /$KIBANACONFIGURATIONDIR/INDEXNAME/kibana_analyzer.json
-  NODE_TLS_REJECT_UNAUTHORIZED=0 elasticdump -XPOST -H'Content-Type: application/json' --input="https://$LOGSTASH_RW_USERNAME:$LOGSTASH_RW_PASSWORD@elasticsearch:9200/.kibana" --output=$ --type=data      > /$KIBANACONFIGURATIONDIR/INDEXNAME/kibana_data.json
+  NODE_TLS_REJECT_UNAUTHORIZED=0
+  elasticdump --headers='{"Content-Type": "application/json"}' --input="https://$LOGSTASH_RW_USERNAME:$LOGSTASH_RW_PASSWORD@elasticsearch:9200/.kibana" --output=/$KIBANACONFIGURATIONDIR/INDEXNAME/kibana_mapping.json --type=mapping
+  elasticdump --headers='{"Content-Type": "application/json"}' --input="https://$LOGSTASH_RW_USERNAME:$LOGSTASH_RW_PASSWORD@elasticsearch:9200/.kibana" --output=/$KIBANACONFIGURATIONDIR/INDEXNAME/kibana_analyzer.json --type=analyzer
+  elasticdump --headers='{"Content-Type": "application/json"}' --input="https://$LOGSTASH_RW_USERNAME:$LOGSTASH_RW_PASSWORD@elasticsearch:9200/.kibana" --output=/$KIBANACONFIGURATIONDIR/INDEXNAME/kibana_data.json --type=data
 fi
 export TESTKIBANAINDEX=''
